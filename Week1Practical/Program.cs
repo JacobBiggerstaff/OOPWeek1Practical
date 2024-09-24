57ea1b97f5b8674a9ffd04791d24917f1ac4e805
Jacob Biggerstaff <Biggerstaff-J1@ulster.ac.uk>
2024-09-24 15:52:51 +0100

    Updating

--- Week1Practical/Program.cs
+++ Week1Practical/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Task 1
+//Console.WriteLine("What is your Name?");
+//String name = Console.ReadLine();
+
+//Console.WriteLine("What is your age?");
+//int age = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("Hello " + name + " you will be " + (age + 5) + " in 5 years");
+
+
+//Task2
+using System.ComponentModel.Design;
+
+//Console.WriteLine("What is your age?");
+//int age = Convert.ToInt32(Console.ReadLine());
+//if (age <= 13)
+//Console.WriteLine("You are a child");
+//if (age > 13 && age <= 19)
+//Console.WriteLine("You are a teenager");
+//if (age > 19 && age <= 110)
+//Console.WriteLine("You are an adult");
+
+
+//Task3
+//Console.WriteLine("Enter a number");
+//int number = Convert.ToInt32(Console.ReadLine());
+//if (number == 0)
+//   Console.WriteLine("Number is 0");
+//    Console.WriteLine("Number is positive");
+//if (number < 0)
+//    Console.WriteLine("Number is negative");
+
+
+
+//Task4 
+//Console.WriteLine("Enter a year");
+//int year = Convert.ToInt32(Console.ReadLine());
+//if (year % 4 == 0 && year % 100 == 0!)
+//    Console.WriteLine("That is a leap year");
+//if (year % 100 == 0 && year % 400 == 0)
+//    Console.WriteLine("Not a leap year");
+//else Console.WriteLine("Not a leap year");
+
+
+//Task5 
+//Console.WriteLine("Please enter a number");
+//int number = Convert.ToInt32(Console.ReadLine());
+//if (number % 2 == 0)
+//    Console.WriteLine("Number is even");
+//else Console.WriteLine("number is odd");
